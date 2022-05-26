d5fdcf29fc8ff34f1a1eb9728b1581908119f699
marc <marc18x@gmail.com>
2022-05-26 20:33:12 +0800

    更新注释信息

--- springmvc-02-hellomvc/src/main/java/com/marc/controller/HelloController.java
+++ springmvc-02-hellomvc/src/main/java/com/marc/controller/HelloController.java
@@ -11,13 +11,22 @@ import javax.servlet.http.HttpServletResponse;
  * @date 2022/5/24
  */
 public class HelloController implements Controller {
+    /**
+     * 处理请求
+     *
+     * @param request
+     * @param response
+     * @return modelAndView对象 交给dispatchServlet进行页面渲染
+     * @throws Exception 异常
+     */
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         // ModelAndView 模型和视图
         ModelAndView mv = new ModelAndView();
 
         // 封装对象，放在ModelAndView中
-        mv.addObject("msg","HelloSpringMVC");
+        mv.addObject("msg", "HelloSpringMVC");
+        mv.addObject("title", "测试页面");
         // 封装要跳转的视图，放在modelAndView
         mv.setViewName("hello");
         return mv;
