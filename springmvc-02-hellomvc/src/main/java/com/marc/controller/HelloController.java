9e7b688c4393c86d6216b22416e7aac7da9dfc94
marc18x <marc18x@gmail.com>
2022-06-01 01:43:30 +0800

    向springmvc-02-hellomvc中添加了一些注解，帮助理解各个类的实际作用（梳理springmvc执行原理）

--- springmvc-02-hellomvc/src/main/java/com/marc/controller/HelloController.java
+++ springmvc-02-hellomvc/src/main/java/com/marc/controller/HelloController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author marc
  * @date 2022/5/24
  */
+// 首先记得导入controller
 public class HelloController implements Controller {
     /**
      * 处理请求
@@ -25,9 +26,11 @@ public class HelloController implements Controller {
         ModelAndView mv = new ModelAndView();
 
         // 封装对象，放在ModelAndView中
+        //  模型负责存数据
         mv.addObject("msg", "HelloSpringMVC");
         mv.addObject("title", "测试页面");
         // 封装要跳转的视图，放在modelAndView
+        // view负责设置跳转的页面
         mv.setViewName("hello");
         return mv;
     }
